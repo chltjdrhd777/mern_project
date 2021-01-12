4ce9b710df79746fa1df2ebc705f34ec1eb2b62c
unknown <chltjdrhd777@naver.com>
2021-01-12 14:47:14 +0900

    done

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- Layout -->
+    <Header />
+
+    <!-- views -->
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Header from "./components/layout/Header";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand href="#">Admin Dashboard</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse">
+        <b-icon-box-arrow-in-right
+          class="header_signin"
+        ></b-icon-box-arrow-in-right>
+      </b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-button
+              size="sm"
+              class="my-2 my-sm-0 header_signin"
+              type="submit"
+              variant="dark"
+              >Signin</b-button
+            >
+          </b-nav-form>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+.header_signin {
+  color: lightcyan;
+}
+</style>
